aa1bf73b72fd2328c19d05725b23378017c715b2
carlabgo <carlabgo95@gmail.com>
2023-02-13 01:04:09 -0300

    agrego componentes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import { ItemDetailContainer } from './components/ItemDetailContainer';
+import { CartContainer } from './components/CartContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 import Hero from './components/Hero';
 
 function App() {
   return (
 <>
-<NavBar background={'transparent'}/>
-<Hero title="FUNKO WORLD"/>
-<ItemListContainer greeting="Conoce nuestros productos"/>
+<CartProvider>
+      <BrowserRouter>
+        <div className="App">
+            <div>
+            <NavBar background={'transparent'}/>
+            <Hero title="FUNKO WORLD"/>
+                <Routes>
+                  <Route path='/' element={<ItemListContainer greeting="Conoce nuestros productos" />}/>
+                  <Route path="/productos/:tipoProducto" element={<ItemListContainer />}/>
+                  <Route path="/item/:productId" element={<ItemDetailContainer/>}/>
+                  <Route path="/cart" element={<CartContainer/>}/>
+             {/*      <Route path='*' element={<ItemListContainer />}/> */}
+                  {/*  // cualquier otra ruta me reenderiza este componente */}
+                </Routes>
+            </div>
+        </div>
+      </BrowserRouter>
+    </CartProvider>
 </>
   );
 }
---
+++ src/components/data/data.js
@@ -0,0 +1,40 @@
+import ariel from '../../img/movies/ariel.png';
+import rapunzel from '../../img/movies/rapuzel.jpeg';
+import mulan from '../../img/movies/mulan.jpeg';
+import snowwhite from '../../img/movies/snowwhite.jpg';
+import belle from '../../img/movies/belle.jpg';
+import lilo from '../../img/movies/lilo.jpg';
+import stitch from '../../img/movies/stitch.jpg';
+import harrypotter from '../../img/movies/harrypotter.jpg';
+import ronweasley from '../../img/movies/ronweasley.jpeg';
+import hermionegranger from '../../img/movies/hermionegranger.jpeg';
+import voldemort from '../../img/movies/voldemort.jpg';
+import superman from '../../img/movies/superman.jpg';
+import batman from '../../img/movies/batman.jpg';
+import wonderwoman from '../../img/movies/wonderwoman.jpg';
+import captainamerica from '../../img/movies/captainamerica.jpeg';
+import thanos from '../../img/movies/thanos.jpg';
+import spiderman from '../../img/movies/spiderman.jpg';
+
+
+export const productsArray = [
+    {id:1, name: "Ariel", price: 30, pictureUrl: ariel, categoria:"movies"},
+    {id:2, name: "Rapunzel", price: 40, pictureUrl: rapunzel, categoria:"movies"},
+    {id:3, name: "Mulan", price: 60, pictureUrl: mulan, categoria:"movies"},
+    {id:4, name: "Snow White", price: 65, pictureUrl: snowwhite,categoria:"movies"},
+    {id:5, name: "Belle", price: 45, pictureUrl: belle,categoria:"movies"},
+    {id:6, name: "Lilo", price: 50, pictureUrl: lilo, categoria:"movies"},
+    {id:7, name: "Stitch", price: 60, pictureUrl: stitch,categoria:"movies"},
+    {id:8, name: "Harry Potter", price: 30, pictureUrl: harrypotter,categoria:"movies"},
+    {id:9, name: "Hermione Grangwer", price: 30, pictureUrl: hermionegranger,categoria:"movies"},
+    {id:10, name: "Ron Weasley", price: 30, pictureUrl: ronweasley,categoria:"movies"},
+    {id:11, name: "Voldemort", price: 30, pictureUrl: voldemort,categoria:"movies"},
+    {id:12, name: "Superman", price: 30, pictureUrl: superman,categoria:"movies"},
+    {id:13, name: "Batman", price: 30, pictureUrl: batman,categoria:"movies"},
+    {id:14, name: "Wonder Woman", price: 30, pictureUrl: wonderwoman,categoria:"movies"},
+    {id:15, name: "Captain America", price: 30, pictureUrl: captainamerica,categoria:"movies"},
+    {id:16, name: "Thanos", price: 30, pictureUrl: thanos,categoria:"movies"},
+    {id:17, name: "Spider-Man", price: 30, pictureUrl: spiderman,categoria:"movies"},
+    
+
+]
